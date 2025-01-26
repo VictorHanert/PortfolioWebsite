0875d36dae4c7a0aed3fccfbae5a7e5c08a3fcdd
VictorHanert <vict660m@stud.kea.dk>
2025-01-26 17:28:38 +0100

    Updated routes and toaster lenght

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
+import resend from "./pages/api/send.ts";
 
 const queryClient = new QueryClient();
 
@@ -15,6 +16,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
+          <Route path="/api/send" element={<resend />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
