bf451d18422474ee9cbf179202740549095fbf19
VictorHanert <vict660m@stud.kea.dk>
2025-01-26 18:03:58 +0100

    Updates

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-import resend from "./pages/api/send.ts";
 
 const queryClient = new QueryClient();
 
@@ -16,7 +15,6 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/api/send" element={<resend />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
