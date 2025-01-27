d3b6f46022dade2ed05c7d8f959edf3754fa93de
VictorHanert <vict660m@stud.kea.dk>
2025-01-27 20:26:21 +0100

    def

--- api/send.ts
+++ api/send.ts
@@ -1,8 +1,9 @@
+import { VercelRequest, VercelResponse } from '@vercel/node';
 import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY!);
 
-export default async function handler(req: any, res: any) {
+export async function handler(req: VercelRequest, res: VercelResponse) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
