b2bc578d06cfaea5aa5adc4675b1c2376a2b6f58
VictorHanert <vict660m@stud.kea.dk>
2025-07-31 22:15:30 +0200

    Updated search field

--- src/components/ProjectSearch.tsx
+++ src/components/ProjectSearch.tsx
@@ -33,7 +33,7 @@ export const ProjectSearch = ({ onSearch }: ProjectSearchProps) => {
             type="text"
             value={searchInput}
             onChange={(e) => setSearchInput(e.target.value)}
-            placeholder="Search (e.g., React, Vue.js)"
+            placeholder="Search by skill"
             className="w-64 sm:w-72 h-full text-xs sm:text-base pl-10 pr-4 py-2 rounded-lg border border-gray-200 focus:outline-none focus:ring-2 focus:ring-primary"
           />
         </div>
