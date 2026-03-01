9a2170f3bec2c83714d6437e7deb1db80e53c03c
VictorHanert <vict660m@stud.kea.dk>
2026-03-01 16:34:30 +0100

    Add @vercel/speed-insights package and integrate SpeedInsights component

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const queryClient = new QueryClient();
 
@@ -20,6 +21,7 @@ const App = () => (
       </BrowserRouter>
     </TooltipProvider>
     <Analytics />
+    <SpeedInsights />
   </QueryClientProvider>
 );
 
