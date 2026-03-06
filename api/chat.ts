5632316aa7c1f04f8de37e654d6eb9cbc5e19c30
VictorHanert <vict660m@stud.kea.dk>
2026-03-06 15:02:04 +0100

    Fix import path

--- api/chat.ts
+++ api/chat.ts
@@ -1,5 +1,5 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
-import { visitedCountries } from "./visitedCountries";
+import { visitedCountries } from "./visitedCountries.js";
 
 const travelKeywords = [
   "travel",
